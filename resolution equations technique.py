8082421ea6a09df309d19961cd2966ee2681b6b1
arosich-prp <55976649+arosich-prp@users.noreply.github.com>
2019-10-11 16:33:24 +0200

    Add files via upload

---
+++ resolution equations technique.py
@@ -0,0 +1,27 @@
+﻿# module des expressions régulières
+import re
+
+# fonction identification des deux membres de l'équation
+def format_equation(equation):
+    # définition du format ax + b = cx + d
+    expression_type=re.compile("(\\-*\d*x *[\\+\\-] *\d*) *= *(\\-*\d*x *[\\+\\-] *\d*)")
+    # vérification du format de l'équation
+    expression=expression_type.match(equation)
+    while expression==None:
+        equation=input("ce n'est pas de la forme ax+b=cx+d, veuillez ressaisir votre équation:")
+        expression=expression_type.match(equation)
+    # identification des deux membres
+    membre1=expression.group(1)
+    membre2=expression.group(2)
+
+# fonction identification du terme en x et de la constante
+def identification(membre):
+    expression_type=re.compile("(\\-*\d*x) *[\\+\\-] *(\d*)")
+    expression=expression_type.match(membre)
+    terme_en_x=expression.group(1)
+    constante=expression.group(2)
+
+# saisie de l'équation à résoudre
+equation=input("saisissez une équation au format ax+b = cx + d:")
+
+
